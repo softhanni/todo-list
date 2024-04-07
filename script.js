2c4529e4f35038cae7db9a9c415e016d84a4d650
Muhammad Hanif <116032896+softhanni@users.noreply.github.com>
2024-04-08 01:09:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 let add = document.querySelector(".add");
 add.addEventListener("click", addtask)
 
-// function addtask (){   
-//     let inp = document.getElementById("input");
-//     let todo = document.getElementsByClassName("todo");
-//     // console.log(todo[0])
-//     if(inp.value !== "" ){
-
-//         todo[0].innerText = inp.value;  
-// }else{
-//     alert("Please Enter Task")
-// }
-// }
-
-
 
 let h3 = document.createElement("h3");
 h3.className = "notask";
@@ -24,11 +11,6 @@ document.querySelector(".alltasks").appendChild(h3);
 function addtask() {
     let inp = document.getElementById("input");
     let todo = document.getElementsByClassName("todo");
-
-    // for(let i = 0; i<todo.length; i++)
-    // console.log(i)
-
-    // let notask = document.querySelector(".notask");
 
 
     if (inp.value !== "" && inp.value.trim() !=="") {
@@ -42,13 +24,7 @@ function addtask() {
             notask.style.display = "none";
 
         }
-        // else{
-        // notask.className = "show";
-        // console.log("hhhhhh")
-
-
-        // }
-
+   
 
 
         let div = document.createElement("div");
@@ -90,76 +66,35 @@ function addtask() {
 
 
 // edit task 
-
-
-
 let list = document.querySelector(".alltasks")
 // console.log(list.children)
+function edittask(event) {
 
+    let taskToEdit = event.target.parentNode.querySelector(".todo");
 
-
-function edittask() {
-
-    let wt = document.querySelector(".todo").innerText;
+    let wt = taskToEdit.innerText;
     let et = prompt("Edit Task", wt);
 
     if (et !== "") {
 
-        document.querySelector(".todo").innerText = et;
+        taskToEdit.innerText = et;
     } else {
         alert("Please write something")
     }
 }
 
+// dellet Task 
 
+function delletTask(event) {
+    let taskToDelete = event.target.parentNode;
+    taskToDelete.remove();
 
-
-function delletTask() {
-
-    let tl = document.querySelector(".tasklist");
-    tl.remove();
-
-    let list = document.querySelector(".alltasks")
-
+    let list = document.querySelector(".alltasks");
     let notask = document.querySelector(".notask");
 
-    // console.log(notask)
-
     if (list.children.length <= 1) {
-
-        // let h3 = document.createElement("h3");
-        // h3.className = "notask";
-        // let h3text = document.createTextNode("No Task");
-        // h3.appendChild(h3text);
-        // document.querySelector(".alltasks").appendChild(h3);
-
-        // notask.style.display = "block"
-        // let notask = document.querySelector(".notask");
-        // notask.className = "show"
         notask.style.display = "block";
-
-
     }
-
-
-
-    // if(list.children.length !== 0 && list.children.length <=1){
-    //     notask.className = "hide";
-
-    //     }
-
-    //     if(list.children.length == 0  || list.children.length <2){
-    //         let notask = document.querySelector(".notask");
-
-    // notask.className = "show";
-    //     console.log("hhh")
-    //     console.log(list.children.length)
-
-    //     }
-    //     else{
-    //         console.log(list.children.length)
-    //     }
-
 }
 
 
